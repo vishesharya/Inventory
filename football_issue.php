c36a28f3b3b93c4ea0fdc98d6a48edb359a8c57c
VISHESH ARYA <apke000@gmail.com>
2024-05-03 15:21:46 +0530

    Update football_issue.php

--- football_issue.php
+++ football_issue.php
@@ -15,14 +15,14 @@ $destination = isset($_POST['destination']) ? $_POST['destination'] : "";
 
 
 // Logic to fetch product names from the database
-$product_query = "SELECT DISTINCT product_name FROM products";
+$product_query = "SELECT DISTINCT product_name FROM products ORDER BY product_name ASC ";
 $product_result = mysqli_query($con, $product_query);
 
 // Logic to fetch product bases and colors based on selected product
 $selected_product = isset($_POST['product_name']) ? $_POST['product_name'] : null;
 if ($selected_product) {
-    $product_base_query = "SELECT DISTINCT product_base FROM products WHERE product_name = '$selected_product'";
-    $product_color_query = "SELECT DISTINCT product_color FROM products WHERE product_name = '$selected_product'";
+    $product_base_query = "SELECT DISTINCT product_base FROM products WHERE product_name = '$selected_product' ORDER BY product_base ASC ";
+    $product_color_query = "SELECT DISTINCT product_color FROM products WHERE product_name = '$selected_product' ORDER BY bladder_color ASC ";
     $product_base_result = mysqli_query($con, $product_base_query);
     $product_color_result = mysqli_query($con, $product_color_query);
 }
