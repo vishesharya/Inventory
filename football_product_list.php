a392670598560cd3f03299443601c362d2dbad1d
VISHESH ARYA <apke000@gmail.com>
2024-05-03 12:17:31 +0530

    Update football_product_list.php

--- football_product_list.php
+++ football_product_list.php
@@ -143,7 +143,8 @@ $result = mysqli_query($con, "SELECT id, product_name, product_base, product_col
                                     <th>Product Base</th>
                                     <th>Product Color</th>
                                     <th>Product Closing Balance</th>
-                              
+                                    <th>Ist Price (Per Piece)</th>
+                                    
                                 </tr>
                             </thead>
                             <tbody>
