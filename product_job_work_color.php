51feb453151b9fcbaef67f1d0f7088e95bd167ae
VISHESH ARYA <apke000@gmail.com>
2024-05-03 15:13:05 +0530

    Update product_job_work_color.php

--- product_job_work_color.php
+++ product_job_work_color.php
@@ -5,12 +5,12 @@ if (isset($_GET['product_name']) && isset($_GET['product_base'])) {
     $product_name = $_GET['product_name'];
     $product_base = $_GET['product_base'];
 
-    $product_color_query = "SELECT DISTINCT kits_job_work FROM kits_product WHERE product_name = '$product_name' AND product_base = '$product_base' AND status = '0'";
+    $product_color_query = "SELECT DISTINCT product_color FROM kits_job_work WHERE product_name = '$product_name' AND product_base = '$product_base' AND status = '0'";
     $product_color_result = mysqli_query($con, $product_color_query);
 
     $colors = array();
     while ($row = mysqli_fetch_assoc($product_color_result)) {
-        $colors[] = $row['product_color'];
+        $colors[] = $row['kits_job_work'];
     }
 
     echo json_encode($colors);
