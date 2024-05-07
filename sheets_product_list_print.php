370c0d819b316eac3d568c35327a50b8ad90d582
VISHESH ARYA <apke000@gmail.com>
2024-05-07 13:25:35 +0530

    Update sheets_product_list_print.php

--- sheets_product_list_print.php
+++ sheets_product_list_print.php
@@ -58,7 +58,7 @@ include_once 'include/admin-main.php';
             $sn = 1;
 
             // Initialize SQL query
-            $q = "SELECT 'product_name', 'product_base', 'product_color', 'remaining_big_panel', 'remaining_small_panel', 'remaining_plain_panel' FROM sheets_product ORDER BY product_name ASC";
+            $q = "SELECT  `product_name`, `product_base`, `product_color`, `remaining_big_panel`, `remaining_small_panel`, `remaining_plain_panel` FROM sheets_product ORDER BY product_name ASC";
 
      
 
