249b6fc3544c0dfb16ee9e39db12d962f139b18e
VISHESH ARYA <apke000@gmail.com>
2024-05-09 16:42:43 +0530

    Update kits_issue_slip_print.php

--- kits_issue_slip_print.php
+++ kits_issue_slip_print.php
@@ -185,7 +185,7 @@ $stitcher_contact = $stitcher_contact_row['stitcher_contact'];
                             <td><?php echo $total_product_quantity; ?></td>
                             <td colspan="2"></td> <!-- Add empty cells for other columns -->
                             <td><?php echo $total_bladder_quantity; ?></td>
-                            <td colspan="2"></td> <!-- Add empty cells for other columns -->
+
                             <td><?php echo $total_thread_quantity; ?></td>
                         </tr>
                     </tfoot>
