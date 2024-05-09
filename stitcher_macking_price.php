6959c977c0496c1f09eb2e5b1acaa0e21988c708
VISHESH ARYA <apke000@gmail.com>
2024-05-09 10:17:35 +0530

    Update stitcher_macking_price.php

--- stitcher_macking_price.php
+++ stitcher_macking_price.php
@@ -13,7 +13,7 @@ $result = null;
 
 $total_ist_price = 0;
 $total_iind_price = 0;
-$total_thread_quantity = 0;
+
 
 // Check if 'View' button is clicked
 if (isset($_POST['view_entries'])) {
@@ -37,8 +37,6 @@ if (isset($_POST['view_entries'])) {
     }
 
     $result = mysqli_query($con, $query);
-
-   
 }
 
 ?>
@@ -199,16 +197,6 @@ if (isset($_POST['view_entries'])) {
 
             $total_ist_price += $ist_price;
             $total_iind_price += $iind_price;
-
-            // Fetch thread quantity for each entry and add to the total
-            $thread_quantity_query = "SELECT thread_quantity FROM kits_job_work WHERE stitcher_name = '$stitcher_name' AND date_and_time BETWEEN '$start_date' AND '$end_date'";
-            $thread_quantity_result = mysqli_query($con, $thread_quantity_query);
-            while ($row = mysqli_fetch_assoc($thread_quantity_result)) {
-                $total_thread_quantity += $row['thread_quantity'];
-                // Calculate total thread quantity
-                
-        
-    }
 
             ?>
             <td><?php echo $ist_price; ?></td>
