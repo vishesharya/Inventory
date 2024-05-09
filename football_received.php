b06949debe43631fab634183ab4cf3f230f1c760
VISHESH ARYA <apke000@gmail.com>
2024-05-09 17:56:07 +0530

    Update football_received.php

--- football_received.php
+++ football_received.php
@@ -55,23 +55,13 @@ if (isset($_POST['challan_no_issue'])) {
     $selected_stitcher = mysqli_real_escape_string($con, $_POST['stitcher_name']); // Added this line
 
     // Query to fetch products based on selected stitcher, challan number, and status = 0
-    $product_query = "SELECT DISTINCT product_name FROM kits_job_work WHERE stitcher_name = '$selected_stitcher' AND challan_no_issue = '$selected_challan' AND status = 0";
+    $product_query = "SELECT DISTINCT product_name,product_base,product_color FROM kits_job_work WHERE stitcher_name = '$selected_stitcher' AND challan_no_issue = '$selected_challan' AND status = 0";
     $product_result = mysqli_query($con, $product_query);
 }
 
-// Fetch product bases based on selected stitcher, challan number, and product name
-if (isset($_POST['product_name'])) {
-    $selected_product = mysqli_real_escape_string($con, $_POST['product_name']);
-    $product_base_query = "SELECT  product_base FROM kits_job_work WHERE stitcher_name = '$selected_stitcher' AND challan_no_issue = '$selected_challan' AND product_name = '$selected_product'";
-    $product_base_result = mysqli_query($con, $product_base_query);
-}
 
-// Fetch product colors based on selected stitcher, challan number, product name, and product base
-if (isset($_POST['product_base'])) {
-    $selected_product_base = mysqli_real_escape_string($con, $_POST['product_base']);
-    $product_color_query = "SELECT DISTINCT product_color FROM kits_job_work WHERE stitcher_name = '$selected_stitcher' AND challan_no_issue = '$selected_challan' AND product_name = '$selected_product' AND product_base = '$selected_product_base'";
-    $product_color_result = mysqli_query($con, $product_color_query);
-}
+
+
 
 if (isset($_POST['add_product'])) {
     // Validate input
