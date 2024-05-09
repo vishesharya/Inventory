3886f1966b02cd81eca1c65abbee433aebaf306b
VISHESH ARYA <apke000@gmail.com>
2024-05-09 13:59:13 +0530

    update security

--- tb_barcode.php
+++ tb_barcode.php
@@ -1,8 +1,11 @@
 <?php
-// Start session
 session_start();
 
-// Include necessary files
+// Check if the user is not logged in, redirect them to the login page
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php"); // Redirect to your login page
+    exit; // Stop further execution
+}
 include_once 'include/connection.php';
 include_once 'include/admin-main.php';
 
--- update_for_kits.php
+++ update_for_kits.php
@@ -1,6 +1,13 @@
 <?php
-// Include your database connection file
+session_start();
+
+// Check if the user is not logged in, redirect them to the login page
+if (!isset($_SESSION['user_id'])) {
+    header("Location: index.php"); // Redirect to your login page
+    exit; // Stop further execution
+}
 include_once 'include/connection.php';
+include_once 'include/admin-main.php';
 
 // Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
