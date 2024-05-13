30772b30b42d23d06dc7c6b6152c7c428fbfbebc
VISHESH ARYA <apke000@gmail.com>
2024-05-13 12:58:16 +0530

    Update sheets_issue_all_slip.php

--- sheets_issue_all_slip.php
+++ sheets_issue_all_slip.php
@@ -74,7 +74,7 @@ if (isset($_POST['view_entries'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
        body {
-            background-color: #f8f9fc;
+            
             font-family: Arial, sans-serif;
         }
         .card {
