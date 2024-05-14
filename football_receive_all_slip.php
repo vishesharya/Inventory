6fed2ff10bba5b4993aea156e4a20e619547bd46
VISHESH ARYA <apke000@gmail.com>
2024-05-14 15:56:29 +0530

    Update football_receive_all_slip.php

--- football_receive_all_slip.php
+++ football_receive_all_slip.php
@@ -216,14 +216,14 @@ if (!empty($selected_challan)) {
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="from_date">From Date:</label>
-                                        <input type="date" class="form-control" id="from_date" name="from_date">
+                                        <input type="date" class="form-control" id="from_date" name="from_date" required>
                                     </div>
                                 </div>
                                 <!-- To date -->
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="to_date">To Date:</label>
-                                        <input type="date" class="form-control" id="to_date" name="to_date">
+                                        <input type="date" class="form-control" id="to_date" name="to_date" required>
                                     </div>
                                 </div>
 
@@ -233,7 +233,7 @@ if (!empty($selected_challan)) {
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="select_stitcher">Select Stitcher:</label>
-                                        <select class="form-select" id="select_stitcher" name="stitcher_name">
+                                        <select class="form-select" id="select_stitcher" name="stitcher_name" required>
          
                                         <option value="">Select Stitcher</option>
                                             <?php while ($row = mysqli_fetch_assoc($stitcher_result)) : ?>
@@ -243,13 +243,10 @@ if (!empty($selected_challan)) {
                                     </div>
                                 </div>
                        
-                              
-                            
-                          
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="select_challan">Select Issue Challan No:</label>
-                                        <select class="form-select" id="select_challan" name="challan_no">
+                                        <select class="form-select" id="select_challan" name="challan_no" required>
                                             <option value="" selected disabled>Select Issue Challan No</option>
                                             <?php if (isset($challan_result_issue)) : ?>
                                                 <?php while ($row = mysqli_fetch_assoc($challan_result_issue)) : ?>
