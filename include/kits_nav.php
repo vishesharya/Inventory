8ce742682ff74acb734ad52ef365a39b8d00c32b
VISHESH ARYA <apke000@gmail.com>
2024-05-14 13:27:28 +0530

    Update kits_nav.php

--- include/kits_nav.php
+++ include/kits_nav.php
@@ -52,7 +52,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark">
   <div class="container">
-    <a class="navbar-brand" href="kits_inventory.php"><img src="./assets/images/go_back.png" alt="icon">Go Back</a>
+    <a class="navbar-brand" href="kits_inventory.php"><img width="25px" src="./assets/images/go_back.png" alt="icon">Go Back</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
