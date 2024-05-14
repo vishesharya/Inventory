9bfc3f8a4bba0070cf847a8167f0fd0a3397e972
VISHESH ARYA <apke000@gmail.com>
2024-05-14 12:16:26 +0530

    Update kits_receive_slip_all_data.php

--- kits_receive_slip_all_data.php
+++ kits_receive_slip_all_data.php
@@ -110,7 +110,7 @@ if (isset($_POST['view_entries'])) {
         }
         .container_slip {
             margin-top: 50px;
-        
+            /* background-color: #f8f9fc; */
             padding: 20px;
             border-radius: 10px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
