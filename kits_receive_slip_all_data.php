fc03681b9ceff7fb234d0cc9c8e3342f35c1216e
VISHESH ARYA <apke000@gmail.com>
2024-05-14 12:05:03 +0530

    Update kits_receive_slip_all_data.php

--- kits_receive_slip_all_data.php
+++ kits_receive_slip_all_data.php
@@ -8,6 +8,13 @@ $labour_query = "SELECT DISTINCT labour_name FROM kits_received ORDER BY labour_
 $labour_result = mysqli_query($con, $labour_query);
 $challan_no = isset($_POST['challan_no']) ? $_POST['challan_no'] : "";
 
+// Fetch the date and time 
+$date_and_time_query = "SELECT date_and_time FROM sheets_issue WHERE challan_no = '$challan_no' LIMIT 1";
+$date_and_time_result = mysqli_query($con, $date_and_time_query);
+$date_and_time_row = mysqli_fetch_assoc($date_and_time_result);
+$date_and_time = isset($date_and_time_row['date_and_time']) ? $date_and_time_row['date_and_time'] : "";
+
+
 
 // Check if 'challan_no' is set in session
 if (isset($_SESSION['challan_no'])) {
