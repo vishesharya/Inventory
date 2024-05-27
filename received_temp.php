693964ef2368527140bac86c6d61f08eaec86a1c
VISHESH ARYA <apke000@gmail.com>
2024-05-27 13:39:53 +0530

    Update received_temp.php

--- received_temp.php
+++ received_temp.php
@@ -372,7 +372,7 @@ if (isset($_POST['submit_products'])) {
                                                 <?php endwhile; ?>
                                             <?php endif; ?>
                                         </select>
-                                    </div>
+                                    </div> </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="quantity">Quantity:</label>
