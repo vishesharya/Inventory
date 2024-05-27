f4ae58a82e56be12fb4f850efc4044123f32f536
VISHESH ARYA <apke000@gmail.com>
2024-05-27 10:19:20 +0530

    Update fatch_challan_no_print_job_work.php

--- fatch_challan_no_print_job_work.php
+++ fatch_challan_no_print_job_work.php
@@ -3,17 +3,17 @@
 include_once 'include/connection.php';
 
 // Fetch parameters from the AJAX request
-$selectedStitcher = $_GET['stitcher'];
+$selectedStitcher = $_GET['labour'];
 $fromDate = $_GET['from_date'];
 $toDate = $_GET['to_date'];
 
 // Sanitize inputs
-$selectedStitcher = mysqli_real_escape_string($con, $selectedStitcher);
+$selectedStitcher = mysqli_real_escape_string($con, $selectedLabour);
 $fromDate = mysqli_real_escape_string($con, $fromDate);
 $toDate = mysqli_real_escape_string($con, $toDate);
 
 // Construct the query to fetch challan numbers based on stitcher and date range
-$query = "SELECT DISTINCT challan_no_issue FROM print_job_work WHERE stitcher_name = '$selectedStitcher' AND date_and_time >= '$fromDate' AND date_and_time <= '$toDate'";
+$query = "SELECT DISTINCT challan_no_issue FROM print_job_work WHERE labour_name = '$selectedLabour' AND date_and_time >= '$fromDate' AND date_and_time <= '$toDate'";
 $result = mysqli_query($con, $query);
 
 // Check if query was successful
