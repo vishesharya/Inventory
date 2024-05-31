93ba4ec38764dfb435f3d52cb3b0fcfe1823dc0a
VISHESH ARYA <apke000@gmail.com>
2024-05-31 15:17:47 +0530

    Update stitcher_macking_price.php

--- stitcher_macking_price.php
+++ stitcher_macking_price.php
@@ -283,7 +283,7 @@ if (isset($_POST['view_entries'])) {
             $total_ist_price += $ist_price;
             $total_iind_price += $iind_price;
             $total_ist_stitches += $data['S_Ist_C_Ist'] + $data['S_Ist_C_IInd']; 
-            $total_iind_stitches += $iind_quality_stitches;
+            $total_iind_stitches += $data['S_IInd_C_Ist'] + $data['S_IInd_C_IInd']; ?></td>
 
             // Reset total thread price for each row
             $total_thread_price = 0;
