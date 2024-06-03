6587a3435db6e15d5763b827d3d8aac01d5aa095
VISHESH ARYA <apke000@gmail.com>
2024-06-03 11:16:31 +0530

    Update kits_received_for_printing.php

--- kits_received_for_printing.php
+++ kits_received_for_printing.php
@@ -397,7 +397,7 @@ if (isset($_POST['submit_products'])) {
                                 <button type="submit" class="btn btn-success" name="submit_products">Submit</button>
                             </div>
                                  <div>
-                                  <a  href="kits_received_slip_print.php" >Print Slip</a>
+                                  <a  href="kits_received_printing_slip_print.php" >Print Slip</a>
                                  </div>
                              
                              </div>
