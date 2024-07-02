978fb21ba98bef34139c57513b8e3c543dd72af4
VISHESH ARYA <apke000@gmail.com>
2024-07-02 17:16:38 +0530

    update

--- kits_inventory.php
+++ kits_inventory.php
@@ -158,6 +158,15 @@ include_once 'include/admin-main.php';
                                         </div>
                                     </div>
                                 </a>
+                                <a href="bladder_inventory.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Bladder Inventory Status</p>
+                                        </div>
+                                    </div>
+                                </a>
+
+                               
 
                                 <a href="kits_product_list.php" style='color:white'>
                                     <div class="col-lg-4">
