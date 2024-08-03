0dcb5405fa28c2f7d1a80e0bbd3d5432d4fe3344
VISHESH ARYA <apke000@gmail.com>
2024-08-03 11:37:37 +0530

    Update tquery_print.php

--- tquery_print.php
+++ tquery_print.php
@@ -16,7 +16,7 @@ include_once 'include/admin-main.php';
             background: white;
             display: block;
             margin: 1.0cm;
-            box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+            
         }
         @media print {
             body, page {
