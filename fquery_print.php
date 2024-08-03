d8ddc843837597be05d0537d1f1d8531a8f43e1e
VISHESH ARYA <apke000@gmail.com>
2024-08-03 12:00:45 +0530

    Update fquery_print.php

--- fquery_print.php
+++ fquery_print.php
@@ -3,214 +3,115 @@ session_start();
 include_once 'include/connection.php';
 include_once 'include/admin-main.php';
 
-$from_date = isset($_POST['from_date']) ? $_POST['from_date'] : '';
-$to_date = isset($_POST['to_date']) ? $_POST['to_date'] : '';
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php include('include/title.php');?> Football Contact Query</title>
+    <title>Football Query Print</title>
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
-
-    <!-- Global stylesheets -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
-    <link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/core.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/components.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/colors.css" rel="stylesheet" type="text/css">
-    <!-- /global stylesheets -->
-
-    <!-- Core JS files -->
-    <script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
-    <script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
-    <!-- /core JS files -->
-
-    <!-- Theme JS files -->
-    <script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
-    <script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
-    <script type="text/javascript" src="assets/js/core/app.js"></script>
-    <script type="text/javascript" src="assets/js/pages/datatables_sorting.js"></script>
-    <!-- /theme JS files -->
-
+    <link href="assets/labels.css" rel="stylesheet" type="text/css">
+    <style>
+        page {
+            background: white;
+            display: block;
+            margin: 1.0cm;
+            box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+        } 
+        @media print {
+            body, page {
+                margin: 0!important;
+                box-shadow: 0;
+                padding:0;
+            }
+        }
+        @page {
+            margin: 0;
+            box-shadow: 0;
+        }
+        .detail-table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        .detail-table th,
+        .detail-table td {
+            padding: 10px;
+            border: 1px solid #000; /* Set border to solid */
+        }
+        .main-heading {
+            font-weight: bold;
+            text-align: right;
+            width: 200px;
+        }
+        .separator {
+            border-top: 2px double #000;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
 </head>
-
 <body>
+    <center>
+        <page size="A4">
+            <?php 
+            if(isset($_POST['submit'])) {
 
-    <!-- Main navbar -->
-    <?php include('include/top.php'); ?>
-    <!-- /main navbar -->
+                $from_date = $_POST['from_date'];
+                $to_date = $_POST['to_date'];
 
-    <!-- Page container -->
-    <div class="page-container">
+                $fdate = date('Y-m-d', strtotime($_POST['from_date']));
+                $tdate = date('Y-m-d', strtotime($_POST['to_date']));
 
-        <!-- Page content -->
-        <div class="page-content">
+                if(empty($_POST['from_date']) && empty($_POST['to_date'])){
+                    $minTime = mysqli_fetch_array(mysqli_query($con,"SELECT MIN(sub_time) as sub_time FROM `contact` WHERE product = 'Football'")); 
+                    $maxTime = mysqli_fetch_array(mysqli_query($con,"SELECT MAX(sub_time) as sub_time FROM `contact` WHERE product = 'Football'")); 
 
-            <!-- Main sidebar -->
-            <?php include('include/left_menu.php'); ?>
+                    $fdate = date('Y-m-d', strtotime($minTime['sub_time'])); 
+                    $tdate = date('Y-m-d', strtotime($maxTime['sub_time'])); 
+                }
 
-            <!-- Main content -->
-            <div class="content-wrapper">
+                $where = " AND sub_time BETWEEN '$fdate' AND '$tdate'";
+            }
 
-                <!-- Page header -->
-                <div class="page-header page-header-default">
-                    <div class="page-header-content">
-                        <div class="page-title">
-                            <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Football Contact Query</span></h4>
-                        </div>
+            $q = "SELECT * FROM contact WHERE product = 'Football' $where ORDER BY sub_time ASC, id ASC";
+            $show = mysqli_query($con, $q);    
 
-                    </div>
+            // Array to keep track of printed mobile numbers
+            $printed_mobiles = array();
+            $unique_mobiles = array();
 
-                    <div class="breadcrumb-line">
-                        <ul class="breadcrumb">
-                            <li><a href="dashboard.php"><i class="icon-home2 position-left"></i> Home</a></li>
-                            <li class="active"><a href="f_query.php" class="btn bg-indigo-300">Football Contact Query</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <!-- /page header -->
+            echo "<table class='detail-table'> <!-- Add class for the table -->
+                    <tr>
+                        <th>Name</th>
+                        <th>Mobile</th>
+                        <th>Email</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>Time</th>
+                    </tr>";
 
-                <!-- Filter and Print form -->
-                <div class="content">
-                    <div class="pad margin no-print">
-                        <div class="callout callout-info">
-                            <form action="f_query.php" method="POST" class="form-horizontal" enctype="multipart/form-data"  autocomplete="off">
-                                <div class="box-body">
-                                    <div class="form-group">
-                                        <label for="inputEmail3" class="col-sm-1 control-label">From Date</label>
-                                        <div class="col-sm-2">
-                                            <input type="date" class="form-control datepicker" name="from_date" id="startdate" value="<?php echo htmlspecialchars($from_date); ?>">
-                                        </div>
-                                        <label for="inputEmail3" class="col-sm-1 control-label">To Date</label>
-                                        <div class="col-sm-2">
-                                            <input type="date" class="form-control" name="to_date" id="enddate" value="<?php echo htmlspecialchars($to_date); ?>">
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <button type="submit" name="filter" class="btn btn-primary">Filter</button>
-                                        </div>
-                                        <div class="col-sm-2">
-                                            <button type="submit" formaction="fquery_print.php" formtarget="_blank" name="print" class="btn btn-warning">Print</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                <!-- /Filter and Print form -->
+            while($data = mysqli_fetch_array($show)) {
+                // Check if mobile number is already printed
+                if(!in_array($data['mobile'], $printed_mobiles)) {
+                    echo "<tr>
+                            <td>".$data['name']."</td>
+                            <td>".$data['mobile']."</td>
+                            <td>".$data['email']."</td>
+                            <td>".$data['city']."</td>
+                            <td>".$data['state']."</td>
+                            <td>".$data['sub_time']."</td>
+                          </tr>";
+                    // Add mobile number to printed list
+                    $printed_mobiles[] = $data['mobile'];
+                }
+            }
 
-                <!-- Table of football contact query -->
-                    <div class="panel panel-flat" style="overflow: auto;">
-                        <div class="panel-heading">
-                            <h5 class="panel-title">Football Contact Query</h5>
-                            <div class="heading-elements">
-                                <ul class="icons-list">
-                                    <li><a data-action="collapse"></a></li>
-                                    <li><a data-action="reload"></a></li>
-                                    <li><a data-action="close"></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <table class="table datatable-multi-sorting">
-                            <thead>
-                                <tr>
-                                    <th>Sn.</th>
-                                    <th>Product Code</th>
-                                    <th>Name</th>
-                                    <th>Mobile</th>
-                                    <th>Email</th>
-                                    <th>City</th>
-                                    <th>State</th>
-                                    <th>Details</th>
-                                    <th class="text-center">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php 
-                                $sn=1;
-                                $query = "SELECT * FROM contact WHERE product = 'Football'";
-
-                                if (!empty($from_date) && !empty($to_date)) {
-                                    $query .= " AND sub_time BETWEEN '$from_date' AND '$to_date'";
-                                }
-
-                                $result = mysqli_query($con, $query);
-                                while($data = mysqli_fetch_array($result)) {
-                                ?>
-                                <tr>
-                                    <td><?php echo $sn; ?>.</td>
-                                    <td><?php echo htmlspecialchars($data['pcode']); ?></td>
-                                    <td><?php echo htmlspecialchars(ucfirst($data['name'])); ?></td>
-                                    <td><?php echo htmlspecialchars(ucfirst($data['mobile'])); ?></td>
-                                    <td><?php echo htmlspecialchars($data['email']); ?></td>
-                                    <td><?php echo htmlspecialchars($data['city']); ?></td>
-                                    <td><?php echo htmlspecialchars($data['state']); ?></td>
-                                    <td>
-                                        <a href="cust_query_more_dtls.php?id=<?php echo htmlspecialchars($data['id']); ?>">
-                                            <input type="button" value="Details" class="btn bg-teal-400">
-                                        </a>
-                                    </td>
-                                    <td class="text-center">
-                                        <ul class="icons-list">
-                                            <li class="dropdown">
-                                                <a href="include/delete_cust_query.php?id=<?php echo htmlspecialchars($data['id']); ?>" onClick="return del();">
-                                                    <img src="assets/images/del.png" style="width:20px;">
-                                                </a>
-                                            </li>
-                                        </ul>
-                                    </td>
-                                </tr>
-                                <?php 
-                                $sn++; 
-                                }  
-                                ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <!-- /Table of football contact query -->
-
-                <!-- Footer -->
-                <?php include('include/footer.php'); ?>
-                <!-- /footer -->
-
-            </div>
-            <!-- /main content -->
-
-        </div>
-        <!-- /page content -->
-
-    </div>
-    <!-- /page container -->
-
-    <!-- Delete/Edit validation -->
-    <script>
-    function del() {
-        var r = confirm('Are you sure want to delete ? ');
-        if (!r) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function edit() {
-        var r = confirm('Are you sure want to edit ?');
-        if (!r) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-    </script>
-    <!-- /Delete/Edit validation -->
-
+            echo "</table>";
+            ?>
+        </page>
+    </center>
+    <script type="text/javascript">
+        window.print();
+    </script>   
 </body>
 </html>
