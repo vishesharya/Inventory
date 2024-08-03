469483a820ae550a201e8cc755dc0643e10247ea
VISHESH ARYA <apke000@gmail.com>
2024-08-03 12:25:51 +0530

    Update football_top.php

--- football_top.php
+++ football_top.php
@@ -3,14 +3,28 @@ session_start();
 include_once 'include/connection.php';
 include_once 'include/admin-main.php';
 
-// Handle form submission
-$from_date = isset($_POST['from_date']) ? $_POST['from_date'] : '';
-$to_date = isset($_POST['to_date']) ? $_POST['to_date'] : '';
+// Initialize filter variables
+$from_date = '';
+$to_date = '';
 
-// Check if form was submitted for filtering
-$filter_query = "";
+// Check if the filter form is submitted
 if (isset($_POST['filter'])) {
-    $filter_query = " AND date_column BETWEEN '$from_date' AND '$to_date'";
+    $from_date = isset($_POST['from_date']) ? $_POST['from_date'] : '';
+    $to_date = isset($_POST['to_date']) ? $_POST['to_date'] : '';
+
+    // Validate date format (optional)
+    if ($from_date && $to_date) {
+        // Make sure the dates are in the correct format and handle SQL injection
+        $from_date = mysqli_real_escape_string($con, $from_date);
+        $to_date = mysqli_real_escape_string($con, $to_date);
+        
+        // Add date filtering to the query
+        $filter_query = " AND date_column BETWEEN '$from_date' AND '$to_date'";
+    } else {
+        $filter_query = "";
+    }
+} else {
+    $filter_query = "";
 }
 
 // Fetch the data with filtering applied
@@ -21,8 +35,10 @@ $query = "SELECT COUNT(*) as count, name, mobile, email, city, state
           ORDER BY count DESC 
           LIMIT 10";
 $result = mysqli_query($con, $query);
-?>
 
+// Debugging: Uncomment the following line to see the SQL query
+// echo "SQL Query: $query";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -72,48 +88,48 @@ $result = mysqli_query($con, $query);
             <!-- Main content -->
             <div class="content-wrapper">
                 
-                    <!-- Page header -->
-                    <div class="page-header page-header-default">
-                        <div class="page-header-content">
-                            <div class="page-title">
-                                <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Football Top 10 Contact Query</span></h4>
-                            </div>
-                        </div>
-                        <div class="breadcrumb-line">
-                            <ul class="breadcrumb">
-                                <li><a href="dashboard.php"><i class="icon-home2 position-left"></i> Home</a></li>
-                                <li class="active"><a href="football_top.php" class="btn bg-indigo-300">Football Top 10 Contact Query</a></li>
-                            </ul>
+                <!-- Page header -->
+                <div class="page-header page-header-default">
+                    <div class="page-header-content">
+                        <div class="page-title">
+                            <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Football Top 10 Contact Query</span></h4>
                         </div>
                     </div>
-                    <!-- /page header -->
-                    <!-- Content area -->
+                    <div class="breadcrumb-line">
+                        <ul class="breadcrumb">
+                            <li><a href="dashboard.php"><i class="icon-home2 position-left"></i> Home</a></li>
+                            <li class="active"><a href="football_top.php" class="btn bg-indigo-300">Football Top 10 Contact Query</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /page header -->
+                
+                <!-- Content area -->
                 <div class="content">
                     <div class="pad margin no-print">
                         <div class="callout callout-info">
-                            <form action="print_football_top.php" method="POST" class="form-horizontal" enctype="multipart/form-data" autocomplete="off">
-								<div class="box-body">
-									<div class="form-group">
-										<label for="from_date" class="col-sm-1 control-label">From Date</label>
-										<div class="col-sm-2">
-											<input type="date" class="form-control datepicker" name="from_date" id="startdate" value="<?php echo $from_date; ?>" >
-										</div>
-										<label for="to_date" class="col-sm-1 control-label">To Date</label>
-										<div class="col-sm-2">
-											<input type="date" class="form-control" name="to_date" id="enddate" value="<?php echo $to_date; ?>" >
-										</div>
-										<div class="col-sm-2">
-											<button type="submit" name="filter" class="btn btn-primary">Filter</button>
-										</div> 
-										<div class="col-sm-2">
-											<button type="submit" formaction="print_football_top.php" formtarget="_blank" name="print" class="btn btn-warning">Print</button>   
-										</div> 
-									</div>
-								</div>			  
-							</form>
+                            <form action="football_top.php" method="POST" class="form-horizontal" enctype="multipart/form-data" autocomplete="off">
+                                <div class="box-body">
+                                    <div class="form-group">
+                                        <label for="from_date" class="col-sm-1 control-label">From Date</label>
+                                        <div class="col-sm-2">
+                                            <input type="date" class="form-control datepicker" name="from_date" id="startdate" value="<?php echo htmlspecialchars($from_date); ?>" >
+                                        </div>
+                                        <label for="to_date" class="col-sm-1 control-label">To Date</label>
+                                        <div class="col-sm-2">
+                                            <input type="date" class="form-control" name="to_date" id="enddate" value="<?php echo htmlspecialchars($to_date); ?>" >
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <button type="submit" name="filter" class="btn btn-primary">Filter</button>
+                                        </div> 
+                                        <div class="col-sm-2">
+                                            <button type="submit" formaction="print_football_top.php" formtarget="_blank" name="print" class="btn btn-warning">Print</button>   
+                                        </div> 
+                                    </div>
+                                </div>              
+                            </form>
                         </div>
                     </div>
-
 
                     <!-- Table -->
                     <div class="panel panel-flat" style="overflow: auto;">
@@ -141,8 +157,6 @@ $result = mysqli_query($con, $query);
                             </thead>
                             <tbody>
                                 <?php 
-                                
-                                $result = mysqli_query($con, "SELECT COUNT(*) as count, name, mobile, email, city, state FROM contact WHERE product = 'Football' GROUP BY mobile ORDER BY count DESC LIMIT 10");
                                 while($data = mysqli_fetch_array($result)) {
                                 ?>
                                     <tr>
@@ -153,7 +167,7 @@ $result = mysqli_query($con, $query);
                                         <td><?php echo $data['city']; ?></td>
                                         <td><?php echo $data['state']; ?></td>
                                         <td>
-                                            <a href="print_football_topdetails.php?mobile=<?php echo $data['mobile']; ?>">
+                                            <a href="print_football_topdetails.php?mobile=<?php echo urlencode($data['mobile']); ?>">
                                                 <input type="button" value="Details" class="btn bg-teal-400">
                                             </a>
                                         </td>
@@ -183,19 +197,11 @@ $result = mysqli_query($con, $query);
     <script>
         function del() {
             var r = confirm('Are you sure want to delete ? ');
-            if (!r) {
-                return false;
-            } else {
-                return true;
-            }
+            return r;
         }
         function edit() {
             var r = confirm('Are you sure want to edit ?');
-            if (!r) {
-                return false;
-            } else {
-                return true;
-            }
+            return r;
         }
     </script>
     <!-- Delete/Edit validation end --> 
