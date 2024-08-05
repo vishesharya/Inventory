e60ea210547702f9132727dff5f45f2a818f9e48
VISHESH ARYA <apke000@gmail.com>
2024-08-05 13:21:43 +0530

    Update admin-main.php

--- include/admin-main.php
+++ include/admin-main.php
@@ -1,84 +1,65 @@
 <?php
-include_once 'connection.php';
+include './include/check_login.php';
+include './include/connection.php';
+class Vishesh_admin {
+    function insert_code($table) {
+        include('connection.php');
+        $this->table_name = $table;
+        $cr_date = date('d M y');
+        $pid = "P-" . rand();
+        $code = mysqli_real_escape_string($con, $_POST['code']);
+        $status = mysqli_real_escape_string($con, $_POST['status']);
+        $result = mysqli_query($con, "INSERT INTO " . $this->table_name . "(code, status, cr_date) VALUES ('$code', '$status', '$cr_date')") or die(mysqli_error($con));
 
-class Vishesh_admin{	
+        if ($result) {
+            return $msg = "pass";
+        } else {
+            return $msg = "fail";
+        }
+    }
 
-function insert_code($table){
-include('connection.php');
-$this->table_name=$table;
+    function select_code($table) {
+        include('connection.php');
+        $this->table_name = $table;
+        $qry = mysqli_query($con, "SELECT * FROM " . $this->table_name . " ORDER BY id DESC") or die("select query fail" . mysqli_error($con));
+        return $qry;
+    }
 
-$cr_date=date('d M y');
+    function select_code_dtls($table, $a) {
+        include('connection.php');
+        $this->table_name = $table;
+        $qry = mysqli_query($con, "SELECT * FROM " . $this->table_name . " WHERE id='$a'") or die("select query fail" . mysqli_error($con));
+        return mysqli_fetch_array($qry);
+    }
 
-$pid="P-".rand();
+    function select_valid_user($table) {
+        include('connection.php');
+        $this->table_name = $table;
+        $user = mysqli_real_escape_string($con, $_POST['username']);
+        $pass = $_POST['password'];
 
+        // Fetch the user data based on the username
+        $qry = mysqli_query($con, "SELECT * FROM $this->table_name WHERE username='$user'") or die('Select query fail: ' . mysqli_error($con));
+        $data = mysqli_fetch_array($qry);
 
-$code=mysqli_real_escape_string($con,$_POST['code']);
-$status=mysqli_real_escape_string($con,$_POST['satus']); 
+        if ($data) {
+            // Verify the password
+            if (password_verify($pass, $data['password'])) {
+                session_start();
+                $_SESSION['admin_logged_in'] = true;
+                $_SESSION['username'] = $data['username'];
+                $_SESSION['name'] = $data['name'];
+                $_SESSION['id'] = $data['id'];
+                $_SESSION['role_user'] = $data['role_user'];
 
-$result=mysqli_query($con,"INSERT INTO ".$this->table_name."(code,status,cr_date)
-       value('$code','$status','$cr_date')") or die(mysqli_error());
-	   
-     			   if($result)
-				   return $msg="pass";
-				   else
-				   return $msg="fail";
-}	
-
-
-//select blog details
-function select_code($table){
-//include('connection.php');
-include('connection.php');
-$this->table_name=$table;
-$qry = mysqli_query($con,"SELECT * FROM ".$this->table_name." order by id desc ") or die("select query fail".mysqli_error());
-  return $qry;
+                header('location:dashboard.php');
+                exit;
+            } else {
+                echo "<script>alert('Username or password is not correct')</script>";
+            }
+        } else {
+            echo "<script>alert('Username or password is not correct')</script>";
+        }
+    }
 }
-
-//select product details
-function select_code_dtls($table,$a){
-//include('connection.php');
-include('connection.php');
-$this->table_name=$table;
-$qry = mysqli_query($con,"SELECT * FROM ".$this->table_name." where id='$a' ") or die("select query fail".mysqli_error());
-  return mysqli_fetch_array($qry);
-}
-
-function slect_valid_user($table){
-//include('connection.php');
-include('connection.php');
-$this->table_name=$table;
-//$user=md5();
-//$pass=md5();
-$user=mysqli_real_escape_string($con,$_POST['username']);
-$pass=mysqli_real_escape_string($con,$_POST['password']);
-
-$qry=mysqli_query($con,"SELECT * FROM $this->table_name WHERE username='$user' AND password='$pass' ") 
-or die('Select query fail'.mysqli_connect_error());	
-$data=mysqli_fetch_array($qry);
-
-if(mysqli_num_rows($qry)==1){ 
-	session_start();
-	$_SESSION['user']="userlogging";
-$_SESSION['username']=$data['username'];
-$_SESSION['name']=$data['name'];
-$_SESSION['id']=$data['id'];
-$_SESSION['role_user']=$data['role_user'];	
-
-header('location:dashboard.php');
-}
-else{
-
-echo "<script>alert('Username or password is not correct')</script>";
-//header('location:index.php');
-}
-}
-
-
-//----------------------useful END-------------------------//
-
-
-}
-//closing of class
-
-
 ?>
