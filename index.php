196449930d7ab6114bf716aac526ee9177c823f7
VISHESH ARYA <apke000@gmail.com>
2024-08-05 13:24:31 +0530

    updated

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
-<?php
-include_once 'include/connection.php';
+<?php 
+session_start();
+error_reporting(0);
+include('include/connection.php');
 include_once 'include/admin-main.php';
-
-if(isset($_POST['admin_login'])){
-    session_start();
-    $check_login = new vishesh_admin();
-    $login = $check_login->slect_valid_user("users");
-}
 ?>
 
 <!DOCTYPE html>
@@ -15,8 +11,9 @@ if(isset($_POST['admin_login'])){
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php include('include/title.php'); ?> - Khanna Sports</title>
+    <title><?php include('include/title.php');?> - Dashboard</title>
     <link rel="icon" type="image/x-icon" href="assets/images/favicon.png">
+
     <!-- Global stylesheets -->
     <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
     <link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
@@ -34,93 +31,131 @@ if(isset($_POST['admin_login'])){
     <!-- /core JS files -->
 
     <!-- Theme JS files -->
+    <script type="text/javascript" src="assets/js/plugins/visualization/d3/d3.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/visualization/d3/d3_tooltip.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
     <script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/ui/moment/moment.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/pickers/daterangepicker.js"></script>
     <script type="text/javascript" src="assets/js/core/app.js"></script>
-    <script type="text/javascript" src="assets/js/pages/login.js"></script>
+    <script type="text/javascript" src="assets/js/pages/dashboard.js"></script>
     <!-- /theme JS files -->
 </head>
-<body class="login-container login-cover">
+
+<body>
+    <!-- Main navbar -->
+    <?php include('include/top.php'); ?>
+    <!-- /main navbar -->
 
     <!-- Page container -->
-    <div class="page-container">
+    <div  class="page-container">
 
         <!-- Page content -->
         <div class="page-content">
 
+            <!-- Main sidebar -->
+            <?php include('include/left_menu.php'); ?>
+            <!-- /main sidebar -->
+
             <!-- Main content -->
             <div class="content-wrapper">
 
-                <!-- Content area -->
-                <div class="content pb-20">
+                <!-- Page header -->
+                <div class="page-header page-header-default">
+                    <div class="page-header-content">
+                        <div class="page-title">
+                            <h4><i class="icon-arrow-left52 position-left"></i> <span class="text-semibold">Home</span> - Dashboard<?php echo $_SESSION['name']; ?></h4>
+                        </div>
+                    </div>
 
-                    <!-- Tabbed form -->
-                    <div class="tabbable panel login-form width-400">
-                        <ul class="nav nav-tabs nav-justified">
-                            <li class="active"><a href="#basic-tab1" data-toggle="tab"><h6>Bar Code Generator</h6></a></li>
+                    <div class="breadcrumb-line">
+                        <ul class="breadcrumb">
+                            <li><a href="./myphp/myphp.php"><i class="icon-home2 position-left"></i> Home</a></li>
+                            <li class="active">Dashboard</li>
                         </ul>
+                    </div>
+                </div>
+                <!-- /page header -->
 
-                        <div class="tab-content panel-body">
-                            <div class="tab-pane fade in active" id="basic-tab1">
-                                <form method="post">
-                                    <div class="text-center">
-                                        <div class="icon-object border-slate-300 text-slate-300"><i class="icon-reading"></i></div>
-                                        <h5 class="content-group">Login to your account <small class="display-block">Your credentials</small></h5>
-                                    </div>
+                <!-- Content area -->
+                <div class="content">
 
-                                    <div class="form-group has-feedback has-feedback-left">
-                                        <input type="text" class="form-control" placeholder="Username" name="username" required="required">
-                                        <div class="form-control-feedback">
-                                            <i class="icon-user text-muted"></i>
+                    <!-- Dashboard content -->
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <!-- Quick stats boxes -->
+                            <div class="row">
+                                <a href="barcode.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Generate Barcode</p>
                                         </div>
                                     </div>
-
-                                    <div class="form-group has-feedback has-feedback-left">
-                                        <input type="password" class="form-control" placeholder="Password" name="password" required="required">
-                                        <div class="form-control-feedback">
-                                            <i class="icon-lock2 text-muted"></i>
+                                </a>
+                                <a href="contact_query.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Contact Query</p>
                                         </div>
                                     </div>
-                                    
-                                    <div class="form-group login-options">
-                                        <div class="row">
-                                            <div class="col-sm-6">
-                                                <label class="checkbox-inline">
-                                                    <input type="checkbox" class="styled" checked="checked">
-                                                    Remember
-                                                </label>
-                                            </div>
-
-                                            <div class="col-sm-6 text-right">
-                                                <a href="forgot.php">Forgot Password</a>
-                                            </div>
+                                </a>
+                                <a href="top_user.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Top 10 Query</p>
                                         </div>
                                     </div>
-
-                                    <div class="form-group">
-                                        <button type="submit" name="admin_login" class="btn bg-blue btn-block">Login <i class="icon-arrow-right14 position-right"></i></button>
+                                </a>
+                               
+                                <a href="check_ratio.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Code Usage Ratio</p>
+                                        </div>
                                     </div>
-                                </form>
+                                </a>
+                                <a href="analytics.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Analytics</p>
+                                        </div>
+                                    </div>
+                              
+                                <a href="inventory.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Inventory</p>
+                                        </div>
+                                    </div>
+                                </a>
+                               
+                                <a href="delete_code.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-pink-400">
+                                            <p style="margin: 10px; font-size: large;">Delete Barcode</p>
+                                        </div>
+                                    </div>
+                                </a>
                             </div>
                         </div>
                     </div>
-                    <!-- /tabbed form -->
+                    <!-- /dashboard content -->
+
+                    <!-- Footer -->
+                    <?php include('include/footer.php'); ?>
+                    <!-- /footer -->
+
                 </div>
                 <!-- /content area -->
+
             </div>
             <!-- /main content -->
+
         </div>
         <!-- /page content -->
+
     </div>
     <!-- /page container -->
-
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QCJH6HJ090"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-QCJH6HJ090');
-    </script>
 </body>
 </html>
