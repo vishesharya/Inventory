16568e0b927830d05cb385fbd293f86dc52dcec2
VISHESH ARYA <apke000@gmail.com>
2024-08-07 10:32:16 +0530

    Update handle_forgot_password.php

--- handle_forgot_password.php
+++ handle_forgot_password.php
@@ -6,22 +6,32 @@ $response = [];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
+    
+    if (empty($email)) {
+        $response['success'] = false;
+        $response['message'] = 'Email is required.';
+        echo json_encode($response);
+        exit();
+    }
 
     $result = $con->query("SELECT * FROM users WHERE username = '$email'");
-    if ($result->num_rows > 0) {
+    if ($result && $result->num_rows > 0) {
         $user = $result->fetch_assoc();
         $token = bin2hex(random_bytes(16));
         $expiry = date('Y-m-d H:i:s', strtotime('+1 hour'));
 
-        $con->query("UPDATE users SET reset_token = '$token', reset_expiry = '$expiry' WHERE username = '$email'");
+        if ($con->query("UPDATE users SET reset_token = '$token', reset_expiry = '$expiry' WHERE username = '$email'")) {
+            $resetLink = "http://khannasports.co.in/reset_password.php?token=$token";
 
-        $resetLink = "http://khannasports.co.in/reset_password.php?token=$token";
+            // Send email with the reset link (pseudo code)
+            // mail($email, "Password Reset", "Click this link to reset your password: $resetLink");
 
-        // Send email with the reset link (pseudo code)
-        // mail($email, "Password Reset", "Click this link to reset your password: $resetLink");
-
-        $response['success'] = true;
-        $response['message'] = 'Password reset link has been sent to your email.';
+            $response['success'] = true;
+            $response['message'] = 'Password reset link has been sent to your email.';
+        } else {
+            $response['success'] = false;
+            $response['message'] = 'Failed to update reset token.';
+        }
     } else {
         $response['success'] = false;
         $response['message'] = 'Email not found.';
