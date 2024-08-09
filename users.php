45c87053b774c83a1642e4eee7ac67581c3833af
VISHESH ARYA <apke000@gmail.com>
2024-08-09 09:16:29 +0530

    Update users.php

--- users.php
+++ users.php
@@ -124,7 +124,7 @@ $result = $con->query("SELECT * FROM users");
 </head>
 <body>
     <header>
-        <h1>Admin Dashboard</h1>
+        <h1>Add / Delete Users</h1>
     </header>
     <div class="container">
         <section>
