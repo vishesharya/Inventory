0d2a0dc0b84145a361667b227ae40eab927fb5a8
VISHESH ARYA <apke000@gmail.com>
2024-08-12 14:21:32 +0530

    Update popup.php

--- popup.php
+++ popup.php
@@ -103,7 +103,9 @@
 <body class="bg-gradient-primary">
 
     <!-- Main content and form elements here -->
-
+    <?php 
+    include 'validation.php';
+    ?>
     <!-- Popup Modal -->
     <div id="popupModal" class="popup">
         <div class="popup-content">
@@ -148,5 +150,6 @@
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 
 </html>
