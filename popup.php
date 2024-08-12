034f293059795b4cdbe7d8af7666a9c42fdc0444
VISHESH ARYA <apke000@gmail.com>
2024-08-12 15:48:43 +0530

    Update popup.php

--- popup.php
+++ popup.php
@@ -160,7 +160,7 @@ h2 {
             </div>
             <div class="popup-right">
                 <h3 class="fw-semibold">Follow Us On Instagram</h3>
-                <p class="fs-1"> Follow us on Instagram "khannasports" and join our community today!</p>
+                <p class="fs-4"> Follow us on Instagram "khannasports" and join our community today!</p>
                 <a href="https://www.instagram.com/khannasports" class="btn btn-outline-primary btn-lg action-btn">Follow Us</a>
             </div>
         </div>
