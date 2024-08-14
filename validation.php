f19edd1ea6a77dde6ee73209c74cae0a0537f78b
VISHESH ARYA <apke000@gmail.com>
2024-08-14 14:56:35 +0530

    Update validation.php

--- validation.php
+++ validation.php
@@ -124,7 +124,7 @@ if (isset($_POST['AddCode'])) {
                             <form action="" method="post"  enctype="multipart/form-data" accept-charset="utf-8" class="user">
                                  <!-- Instagram Note -->
                             <p style="color: #0056b3; font-size: medium; text-align: center;">
-                                Get a voucher worth INR 250! Follow us on Instagram and enter your Instagram username below to verify.
+                              Follow us on Instagram and enter your Instagram username below to verify.
                             </p>
 
                             <!-- Instagram Username Field -->
