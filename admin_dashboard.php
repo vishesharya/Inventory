8bf5d300bcc7f3c1baace182222f95c5e6db66d9
VISHESH ARYA <apke000@gmail.com>
2024-08-17 12:35:19 +0530

    Update admin_dashboard.php

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,7 +2,7 @@
 include './include/check_login.php';
 include './include/connection.php';
 include_once 'include/admin-main.php';
-include('access_control.php');
+
 
 ?>
 
@@ -117,7 +117,7 @@ include('access_control.php');
                                         </div>
                                     </div>
                                 </a>
-                                
+
                                 <a href="guards.php" style='color:white'>
                                     <div class="col-lg-4">
                                         <div class="panel bg-pink-400">
