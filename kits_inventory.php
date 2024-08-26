4b1354225301bb5b5b0c3f1c149b79f9632cd3bf
VISHESH ARYA <apke000@gmail.com>
2024-08-26 16:35:26 +0530

    Update kits_inventory.php

--- kits_inventory.php
+++ kits_inventory.php
@@ -100,6 +100,13 @@ include_once 'include/admin-main.php';
                                         </div>
                                     </div>
                                 </a>
+                                <a href="kits_receive_form.php" style='color:white'>
+                                    <div class="col-lg-4">
+                                        <div class="panel bg-teal-400">
+                                            <p style="margin: 10px; font-size: large;">Return Form</p>
+                                        </div>
+                                    </div>
+                                </a>
                                 <a href="kits_inventory_without_print.php" style='color:white'>
                                     <div class="col-lg-4">
                                         <div class="panel bg-teal-400">
