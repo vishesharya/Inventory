01dce9e98b0a7f4d299656dd2ea899432d253c7c
VISHESH ARYA <apke000@gmail.com>
2024-08-26 11:03:43 +0530

    Update kits_issue_slip_print.php

--- kits_issue_slip_print.php
+++ kits_issue_slip_print.php
@@ -233,7 +233,7 @@ $stitcher_contact = $stitcher_contact_row['stitcher_contact'];
             </div>
             <div class="middle-signature">Guard Signature <br>
             <?php if ($signature_file_path): ?>
-                    <img src="<?= htmlspecialchars($signature_file_path) ?>" alt="Guard Signature" style="width: 150px; height: 50px;">
+                    <img src="<?= htmlspecialchars($signature_file_path) ?>" alt="Guard Signature" style="width: 170px; height: 70px;">
                 <?php else: ?>
                     <p>No signature available.</p>
                 <?php endif; ?>
